c4804ccd46d02edfd430e67afffb397dd207dfab
Brishti <you@example.com>
2020-11-30 10:16:06 -0500

    update

--- PieChart.js
+++ PieChart.js
@@ -1,111 +1,213 @@
-export default function PieChart(container){
+export default function PieChart(container) {
     // initialization
-    var margin = {top: 0, right: 10, bottom: 100, left: 60},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-
-    const svg = d3.select(container)
-        .append('svg')
-        .attr('width', width + margin.left + margin.right)
-        .attr('height', height + margin.top + margin.bottom)
-        .append('g')
-        .attr('transform', 'translate('+width/2+','+height/2+')')
-
+    var margin = { top: 0, right: 10, bottom: 100, left: 60 },
+      width = 960 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
+  
+    const svg = d3
+      .select(container)
+      .append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
+  
     const group = svg
-        .append('g')
-        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-    function update(data,type){
-
-        var genres = [];
-
-        var doNothing=0;
-        var total=0;
-        for (var i=0;i<data.length;i++){
-            if (data[i].Age===type){
-                total++;
-                if (data[i].Genres==null){
-                    doNothing++;
-                }
-                else{
-                    var temp =data[i].Genres.split(',');
-                    for(var j=0;j<temp.length;j++){
-                        genres.push(temp[j]);
-                    }
-                }
-            }
+      .append('g')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+  
+    function update(data, type,platform) {
+  
+      var dataSelect = [];
+  
+      for (var i = 0; i < data.length; i++) {
+        if (platform==='All'&& data[i].Age==type){
+          dataSelect.push({
+            Age: data[i].Age,
+            Genres: data[i].Genres
+          })
         }
-        
-        const select ={};
-        genres.forEach((el) => {
-            if (select[el]){
-                select[el]++;
+        else if (platform==='Netflix'&& data[i].Age==type){
+          if (data[i].Netflix==1){
+            dataSelect.push({
+              Age: data[i].Age,
+              Genres: data[i].Genres
+            })
+          }
+        }
+        else if (platform==='Hulu'&& data[i].Age==type){
+          if (data[i].Hulu==1){
+            dataSelect.push({
+              Age: data[i].Age,
+              Genres: data[i].Genres
+            })
+          }
+        }
+        else if (platform==='Prime Video'&& data[i].Age==type){
+          if (data[i]['Prime Video']==1){
+            dataSelect.push({
+              Age: data[i].Age,
+              Genres: data[i].Genres
+            })
+          }
+        }
+        else if (platform==='Disney+'&& data[i].Age==type){
+          if (data[i]['Disney+']==1){
+            dataSelect.push({
+              Age: data[i].Age,
+              Genres: data[i].Genres
+            })
+          }
+        }
+      }
+  
+      var genres = [];
+      var doNothing = 0;
+
+      for (var i = 0; i < dataSelect.length; i++) {
+          if (dataSelect[i].Genres == null) {
+            doNothing++;
+          } else {
+            var temp = dataSelect[i].Genres.split(',');
+            for (var j = 0; j < temp.length; j++) {
+              genres.push(temp[j]);
             }
-            else{
-                select[el]=1;
-            }
+          }
+      }
+  
+      var sortGenre = []
+      genres.forEach((d) => {
+        let genre = d;
+  
+        if (
+          genre == 'Action' ||
+          genre == 'Adventure' ||
+          genre == 'Sci-Fi' ||
+          genre == 'Fantasy'
+        ) {
+          sortGenre.push('Action & Adventure');
+        } else if (
+          genre == 'Romance' ||
+          genre == 'Drama' 
+        ) {
+          sortGenre.push('Romance & Drama');
+        } else if (
+          genre == 'Biography' || 
+          genre == 'War' || 
+          genre == 'History' || 
+          genre == 'News' || 
+          genre == 'Sport' || 
+          genre == 'Short' || 
+          genre == 'Western' || 
+          genre == 'Documentary'
+        ) {
+          sortGenre.push('Biography & News');
+        } else if (
+          genre == 'Horror' ||
+          genre == 'Mystery' ||
+          genre == 'Thriller' ||
+          genre == 'Crime' ||
+          genre == 'Film-Noir'
+        ) {
+          sortGenre.push('Horror & Thriller');
+        } else if (
+          genre == 'Music' ||
+          genre == 'Family' ||
+          genre == 'Musical' ||
+          genre == 'Animation'
+        ) {
+          sortGenre.push('Family & Musical');
+        } else {
+          sortGenre.push('Other');
+        }
+      });
+  
+      console.log(sortGenre);
+  
+      const select = {};
+      sortGenre.forEach((el) => {
+        if (select[el]) {
+          select[el]++;
+        } else {
+          select[el] = 1;
+        }
+      });
+  console.log(select);
+      var data1 = [];
+  
+      for (var key in select) {
+        data1.push({
+          genre: key,
+          value: select[key]
+          //percent: parseInt((select[key] / total) * 100),
         });
-
-
-        var data1 = [];
-
-        for (var key in select) {
-            data1.push({
-                genre: key,
-                value: select[key],
-                percent: parseInt(select[key]/total*100)
-            })
-        };
-
-        const arc = d3.arc()
-            .innerRadius(0)
-            .outerRadius(150)
-
-        var pie = d3.pie().value(function(d) {
-            return d.value;
-          });
-        //console.log(pie(data1));
-        
-        const color = d3.scaleOrdinal()
-            .domain(data1)
-            .range(['#D35231', '#C38231','#E6FF80', '#1AFF33', '#999933',
-            '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
-            '#E64D66', '#4DB380', '#FF4D4D', '#98E6E6', '#6666FF']);
-        
-        let piechart = group.datum(data1).selectAll('path')
-            .data(pie);
-        piechart
-            .enter()
-            .append('path')
-            .merge(piechart)
-            .on("mouseenter", (event,d) =>{
-                const pos = d3.pointer(event, window);
-                d3.select('.tooltip')
-                  .style('display', 'inline-block')
-                  .style('top', pos[1] + 'px')
-                  .style('left', pos[0] + 'px')
-                  .html( 
-                    'Genre: ' +d.data.genre 
-                    + '<br>' + 'Number of movies: '+ d.data.value 
-                    + '<br>' + 'Percent: '+ d.data.percent + '%'
-                  );
-             })
-            .on("mouseleave", (event, d) => {
-                d3.select('.tooltip').style('display', 'none'); 
-            })
-            .transition()
-            .style("fill", function(d, i) {
-                return color(i);
-              })
-            .attr('d',arc);
-
-        piechart.exit().remove();
-        
-
-
-    };
+      }
+      console.log(select);
+  
+      const arc = d3.arc().innerRadius(0).outerRadius(150);
+  
+      var pie = d3.pie().value(function (d) {
+        return d.value;
+      });
+      console.log(data1);
+  
+      const color = d3
+        .scaleOrdinal()
+        .domain(data1)
+        .range([
+          '#D35231',
+          '#C38231',
+          '#E6FF80',
+          '#1AFF33',
+          '#999933',
+          '#FF3380',
+          '#CCCC00',
+          '#66E64D',
+          '#4D80CC',
+          '#9900B3',
+          '#E64D66',
+          '#4DB380',
+          '#FF4D4D',
+          '#98E6E6',
+          '#6666FF',
+        ]);
+  
+      let piechart = group.datum(data1).selectAll('path').data(pie);
+      piechart
+        .enter()
+        .append('path')
+        .merge(piechart)
+        .on('mouseenter', (event, d) => {
+          const pos = d3.pointer(event, window);
+          d3.select('.tooltip')
+            .style('display', 'inline-block')
+            .style('top', pos[1] + 'px')
+            .style('left', pos[0] + 'px')
+            .html(
+              'Genre: ' +
+                d.data.genre +
+                '<br>' +
+                'Number of movies: ' +
+                d.data.value +
+                '<br>' +
+                'Percent: ' +
+                d.data.percent +
+                '%'
+            );
+        })
+        .on('mouseleave', (event, d) => {
+          d3.select('.tooltip').style('display', 'none');
+        })
+        .transition()
+        .style('fill', function (d, i) {
+          return color(i);
+        })
+        .attr('d', arc);
+  
+      piechart.exit().remove();
+    }
     return {
-        update
-	};    
-
-}
+      update,
+    };
+  }
+  
--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 import PieChart from './PieChart.js';
 
-d3.csv('final_pj_dataset/MoviesOnStreamingPlatforms_updated.csv', d3.autoType).then(data=>{
+d3.csv('MoviesOnStreamingPlatforms_updated.csv', d3.autoType).then((data) => {
+  let type = 'all';
+  let platform ='All';
+  d3.select('#group-by').on('change', (e) => {
+    type = e.target.value;
+    pieChart.update(data, type,platform);
+  });
+  d3.select('#platform').on('click', (e) => {
+    platform = e.target.value;
+    pieChart.update(data, type,platform);
+  });
 
-    let type = 'all';
-    d3.select('#group-by')
-      .on('change', e => {
-        type = e.target.value;
-        pieChart.update(data, type);
-      });
-    console.log(data);
-    const pieChart = PieChart(".pie"); 
-    pieChart.update(data,type);
-})
+  const pieChart = PieChart('#pie-chart-container');
+  pieChart.update(data, type,platform);
+});
